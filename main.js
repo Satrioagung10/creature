a8a8db8a21e5c57a1ac4b7267f4e2292f31cfd18
Satrio Agung Wicaksono <razorcallahan54@gmail.com>
2023-10-19 02:14:00 +0700

    update

--- main.js
+++ main.js
@@ -1,24 +1,22 @@
 $(document).ready(function() {
 
-    
     $('#dropdown').click(function() {
         if ($('#dropdown-content').css('display') === 'none') {
             $('#dropdown-content').css('display', 'block');
             $('#arrow').html(
                 '<i class="fas fa-angle-up ms-1" style="font-size:18px"></i>'
             );
+            
         } else {
             $('#dropdown-content').css('display', 'none');
             $('#arrow').html(
                 '<i class="fas fa-angle-down ms-1" style="font-size:18px"></i>'
             );
+            
         }
     });
 
     $('.whatsapp').click(function() {
-
-        
-
         if ($('.popup').css('display') === 'none') {
             $('.whatsapp').html(
                 '<i class="bi bi-x" style="font-size:33px;"></i>');
