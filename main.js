6d134e5f54ac2438fd7a59b7df4fa2444f09b4f0
Satrio Agung Wicaksono <razorcallahan54@gmail.com>
2023-10-21 02:59:30 +0700

    update

--- main.js
+++ main.js
@@ -74,7 +74,26 @@ $(document).ready(function () {
   });
 
   $("#logo").click(function () {
-    $("#content").html("logo");
+    $("#content").html(
+      ` 
+      <div class="img-line1 flex flex-col md:flex-row justify-center gap-5">
+      <div class="img-1">
+        <img src="https://media.discordapp.net/attachments/387127158935388160/1164989163242799164/img2-1.png?ex=654537b4&is=6532c2b4&hm=67a02dd53ef1554225bc8dcb70666caef80d9779a4e7c0444ce8e4f8f90e6a87&=&width=768&height=448" alt="1">
+      </div>
+      <div class="img-2 flex flex-col gap-5">
+        <img src="https://media.discordapp.net/attachments/387127158935388160/1164989163528007720/img2-2.png?ex=654537b5&is=6532c2b5&hm=a2815d4a211442a91f30da787621ae231b69adf062cf88c54d17beb6b5499e71&=&width=356&height=215" alt="2">
+        <img src="https://media.discordapp.net/attachments/387127158935388160/1164989163783852182/img2-3.png?ex=654537b5&is=6532c2b5&hm=ab75e01d9d7e582caaf0331d1070b56fbce852a9a4055ac9e46eb55386eb8d6b&=&width=356&height=208" alt="3">
+      </div>
+    </div>
+
+    <div class="img-line2 flex sm:justify-center mt-5 gap-4 md:gap-6 lg:gap-8">
+      <img class="hidden md:block" src="https://media.discordapp.net/attachments/387127158935388160/1164989164031324160/img2-4.png?ex=654537b5&is=6532c2b5&hm=5b9a67cf6ced3da049da7348f6b76a7b3b82dccbd96d84b7ddb5bdabde0e4508&=&width=256&height=287" alt="4">
+      <img class="hidden md:block" src="https://media.discordapp.net/attachments/387127158935388160/1164989164551409736/img2-5.png?ex=654537b5&is=6532c2b5&hm=3a59619ebaa01a0a0cf75cd410dd5517994f263e4e3751013e73caa948ee2fe1&=&width=256&height=287" alt="5">
+      <img class="hidden md:block" src="https://media.discordapp.net/attachments/387127158935388160/1164989165000212621/img2-6.png?ex=654537b5&is=6532c2b5&hm=aaab96b7ab40be51007b63a9fe668f691c908836fb860d0d92758a10c89d38c9&=&width=256&height=287" alt="6">
+      <img class="hidden lg:block" src="https://media.discordapp.net/attachments/387127158935388160/1164989165339947228/img2-7.png?ex=654537b5&is=6532c2b5&hm=d515bb12540f65ecb66620f1f53b24224cec15da37f4f51a0e6d6f00868bcec6&=&width=256&height=287" alt="7">
+    </div>     
+     `
+    );
   });
 
   $(document).ready(function () {
@@ -89,3 +108,6 @@ $(document).ready(function () {
   });
 });
 
+function newWindow(url){
+  window.open(url, '_blank');
+}
